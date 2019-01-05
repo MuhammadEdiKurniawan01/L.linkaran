475295c7a3c565d5fc112434bd0b8df4cd448bea
MuhammadEdiKurniawan01 <edikurunyawan25@gmail.com>
2019-01-05 22:31:26 +0700

    lingkatan

---
+++ lingkaran.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int r;
+    float phi=3.14,luas;
+
+    cout<<"masukan jari jari lingkaran : ";
+    cin>>r;
+    cout<<"           Luas = phi X r X r \n";
+    luas=phi*r*r;
+    cout<<"luas lingkaran adalah :"<<luas;
+    return 0;
+}
